a0530fff43836037fe04cc290c90516663b6d306
Cristoffer Solem <cristoffer.braadlie.solem@nrk.no>
2019-01-24 10:30:37 +0100

    Move generate invitations to cron.

--- index.js
+++ index.js
@@ -123,35 +123,9 @@ var members = [];
 // Handle events related to the websocket connection to Slack
 controller.on('rtm_open', function (bot) {
     console.log('** The RTM api just connected!');
-    bot.api.channels.info({channel: CHANNEL}, function(err, list) {
-        members = list.channel.members;
-        console.log("Fetched members: ", members);
-    
-        let randomMembers = findRandomMembers(GROUP_SIZE, members).map((id) =>({id}));
-        //let randomMembers = [{id: "UDU6KE0A0"}];
-        console.log("Generated a list of random members: ", randomMembers);
-        let replies = inviteUsers(bot, randomMembers);
 
-      replies.then((users)=>{
-        const acceptedUsers = users.filter(Boolean);
-        console.log(`choosen users: ${acceptedUsers}`);
-        if(acceptedUsers.length === 0 ) {
-            console.log("We need more peops in the group", acceptedUsers);
-        let rejectedUsersString = userMentions(randomMembers);
-        bot.say({channel: CHANNEL, text: `I dag var det ingen som ville fika :cry:, fy fabian for er: ${rejectedUsersString}`})
-            return; 
-        }
-
-        let usersString = userMentions(acceptedUsers);
-
-        // Random buyer. Poor person.
-        let responsibleUser = userMention(acceptedUsers[0]);        
-        //let responsibleUser = userMention({id: "UDU6KE0A0"});
-
-        console.log("This person is chosen to buy stuff:", responsibleUser);
-        bot.say({channel: CHANNEL, text: `Tjabba tjena allihopa!\n Klockan 14:00 skall ${usersString} fika tillsammans i lunchrummet på andra våningen i glasgården :tada:\n ${responsibleUser} lånar kantinakortet från Lena och köper fika. NRK betalar.\n Blev det inte din tur idag?\n Du får en ny chans i morgon :nerd_face:`})
-      })
-    });
+    startGenerateFikaCrontab(bot);
+    startRemindersCrontab(bot);
 });
 
 function userMentions(users) {
@@ -176,58 +150,98 @@ function invitationExists() {
 function readInvitation() {
     return JSON.parse(fs.readFileSync(INVITATION_FILE));
 }
-function startCrontab(bot) {
-cron.schedule('* * * * *', () => {  
-    console.log(pendingInvitations)
-  pendingInvitations.forEach((pendingInvitation) => {
-    const {user, conversation, resolve} = pendingInvitation;
-    if (pendingInvitation.isRepeated) {
-      conversation.repeat();
-      conversation.next();
-      return;
-    }
-    conversation.isRepeated = true;
 
-    conversation.addQuestion('Horru blir\'u med eller inte? Plz svara :smile:',[
-      {
-        pattern: /^ja/i,
-        callback: function(response,convo) {
-          convo.say('Jättekul! Det här blir hur bra som helst');
-          convo.next();
-          pendingInvitations = pendingInvitations.filter((pendingInvitation) => pendingInvitation.user.id != user.id);
-          resolve(user);
-        }
-      },
-      {
-        pattern: /^ne[ij]/i,
-        callback: function(response,convo) {
-          convo.say('Tråkmåns.. jaja kanske en annan gång då :D');
-          convo.next();
-          resolve();
-        }
-      },
-      {
-        default: true,
-        callback: function(response,convo) {
-          convo.say('Nu fattade jag inte helt var du sa kompis');
-          convo.silentRepeat();
-          convo.next();
-        }
-      }
-    ]);
-    conversation.next();
-  })
+/**
+ * At every 10th minute past every hour from 9 through 11 on every day-of-week from Monday through Thursday.
+ * When testing is done, change "every 10th minute" to one time per day.
+ */
+//cron.schedule('*/10 9-11 * * 1-4'
+function startGenerateFikaCrontab(bot) {
+    cron.schedule('* 9-11 * * 1-4', () => {  
+        bot.api.channels.info({channel: CHANNEL}, function(err, list) {
+            members = list.channel.members;
+            console.log("Fetched members: ", members);
 
-    if (invitationExists()) {
-        
-        let invitation = readInvitation();
-        console.log("Found invitation: ", invitation);
-        
-        // Determine if it's about to send 14:00 notification in channel
-    } else {
-        console.log("No invitation exist");
-    }
-});
+            let randomMembers = findRandomMembers(GROUP_SIZE, members).map((id) =>({id}));
+            //let randomMembers = [{id: "UDU6KE0A0"}];
+            console.log("Generated a list of random members: ", randomMembers);
+            let replies = inviteUsers(bot, randomMembers);
+
+            replies.then((users)=>{
+                const acceptedUsers = users.filter(Boolean);
+                console.log(`choosen users: ${acceptedUsers}`);
+                if(acceptedUsers.length === 0 ) {
+                    console.log("We need more peops in the group", acceptedUsers);
+                let rejectedUsersString = userMentions(randomMembers);
+                bot.say({channel: CHANNEL, text: `I dag var det ingen som ville fika :cry:, fy fabian for er: ${rejectedUsersString}`})
+                    return; 
+                }
+
+                let usersString = userMentions(acceptedUsers);
+
+                // Random buyer. Poor person.
+                let responsibleUser = userMention(acceptedUsers[0]);        
+                //let responsibleUser = userMention({id: "UDU6KE0A0"});
+
+                console.log("This person is chosen to buy stuff:", responsibleUser);
+                bot.say({channel: CHANNEL, text: `Tjabba tjena allihopa!\n Klockan 14:00 skall ${usersString} fika tillsammans i lunchrummet på andra våningen i glasgården :tada:\n ${responsibleUser} lånar kantinakortet från Lena och köper fika. NRK betalar.\n Blev det inte din tur idag?\n Du får en ny chans i morgon :nerd_face:`})
+            })
+        });
+    });
+}
+
+function startRemindersCrontab(bot) {
+    cron.schedule('* * * * *', () => {  
+        console.log(pendingInvitations)
+        pendingInvitations.forEach((pendingInvitation) => {
+            const {user, conversation, resolve} = pendingInvitation;
+            if (pendingInvitation.isRepeated) {
+            conversation.repeat();
+            conversation.next();
+            return;
+            }
+            conversation.isRepeated = true;
+
+            conversation.addQuestion('Horru blir\'u med eller inte? Plz svara :smile:',[
+            {
+                pattern: /^ja/i,
+                callback: function(response,convo) {
+                convo.say('Jättekul! Det här blir hur bra som helst');
+                convo.next();
+                pendingInvitations = pendingInvitations.filter((pendingInvitation) => pendingInvitation.user.id != user.id);
+                resolve(user);
+                }
+            },
+            {
+                pattern: /^ne[ij]/i,
+                callback: function(response,convo) {
+                convo.say('Tråkmåns.. jaja kanske en annan gång då :D');
+                convo.next();
+                resolve();
+                }
+            },
+            {
+                default: true,
+                callback: function(response,convo) {
+                convo.say('Nu fattade jag inte helt var du sa kompis');
+                convo.silentRepeat();
+                convo.next();
+                }
+            }
+            ]);
+            conversation.next();
+        })
+
+        if (invitationExists()) {
+            
+            let invitation = readInvitation();
+            console.log("Found invitation: ", invitation);
+            
+            // Determine if it's about to send 14:00 notification in channel
+        } else {
+            console.log("No invitation exist");
+        }
+    });
 }
 
 
